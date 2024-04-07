52b64f3054aae9310805e0b9889d03806d858fdf
JoepvanDam <joep.vandam@student.hu.nl>
2024-04-07 11:08:18 +0200

    Added contact billboard + styling

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Imports
 import { switchProjects, openProject } from "./modules/projects.js";
+import { toGmail, toLinkedIn, toGithub } from "./modules/links.js";
 import animationSwitch from "./modules/animSwitch.js";
 import languageSwitch from "./modules/langSwitch.js";
 import stickyTalks from "./modules/stickyTalks.js";
@@ -18,8 +19,11 @@ window.darkModeSwitch = darkModeSwitch;
 window.switchProjects = switchProjects;
 window.openSettings = openSettings;
 window.openProject = openProject;
+window.toLinkedIn = toLinkedIn;
 window.switchTabs = switchTabs;
 window.sendPlane = sendPlane;
+window.toGithub = toGithub;
+window.toGmail = toGmail;
 
 // Global variables
 var currentTalkNum = 0;
@@ -37,7 +41,7 @@ function checkPortrait() {
 // On start
 // darkModeSwitch(true);
 updateStickyTalks(0);
-switchTabs(2);
+switchTabs(3);
 checkPortrait();
 switchProjects(0);
 updateDates();
---
+++ modules/links.js
@@ -0,0 +1,4 @@
+function toGmail() { window.open('mailto: joepportfoliobusiness@gmail.com?Subject=Portfolio Contact'); }
+function toLinkedIn() { window.open('https://www.linkedin.com/in/joep-van-dam-5a0072226 ', '_blank'); }
+function toGithub() { window.open('https://www.github.com/JoepvanDam ', '_blank'); }
+export { toGmail, toLinkedIn, toGithub };
